1a337112c0618063f9f614d5a671a03a3b466280
Jesper Oskarsson <jesosk@gmail.com>
2015-10-31 16:17:07 +0100

    Added utility functions to handle strings

--- unit/javascript-translator.unit.js
+++ unit/javascript-translator.unit.js
@@ -259,4 +259,53 @@ describe('Javascript translator', function () {
             'var f = (function () { return vector_58last_45element(vector_58new()); });'
         );
     });
+
+    describe('stringToCodePointList', function () {
+        it('should return an empty vector given an empty string', function () {
+            var result = translator.listToArray(translator.stringToCodePointList(''));
+            result.should.have.a.lengthOf(0);
+        });
+
+        it('should return a vector with the unicode code point of the first character in the string', function () {
+            var result = translator.listToArray(translator.stringToCodePointList('金'));
+            result.should.have.a.property(0, 37329);
+        });
+
+        it('should return a vector with the unicode code points of all of the characters in the string', function () {
+            var result = translator.listToArray(translator.stringToCodePointList('金魚魚魚'));
+            result.should.have.a.property(0, 37329);
+            result.should.have.a.property(1, 39770);
+            result.should.have.a.property(2, 39770);
+            result.should.have.a.property(3, 39770);
+        });
+
+        it('should convert all code points over 0x10000 correctly', function () {
+            var result = translator.listToArray(translator.stringToCodePointList('\uD800\uDC00'));
+            result.should.have.a.property(0, 65536);
+        });
+    });
+
+    describe('listToArray', function () {
+        it('should return an empty array given nil', function () {
+            var result = translator.listToArray(null);
+            result.should.be.instanceOf(Array);
+            result.should.have.a.lengthOf(0);
+        });
+
+        it('should return an array with the first item in the list given a list with one item', function () {
+            var result = translator.listToArray([1, null]);
+            result.should.be.instanceOf(Array);
+            result.should.have.a.property(0, 1);
+            result.should.have.a.lengthOf(1);
+        });
+
+        it('should return an array with all items given a list with several items', function () {
+            var result = translator.listToArray([1, [3, [2, null]]]);
+            result.should.be.instanceOf(Array);
+            result.should.have.a.lengthOf(3);
+            result.should.have.a.property(0, 1);
+            result.should.have.a.property(1, 3);
+            result.should.have.a.property(2, 2);
+        });
+    });
 });
