31b3807dacaa9534cb466b8c6ea6e83f7704a2e7
Jesper Oskarsson <jesosk@gmail.com>
2016-06-14 20:40:10 +0200

    factored out infix operators for languages with 32 bit integers

--- rust/translator.js
+++ rust/translator.js
@@ -1,26 +1,9 @@
 
 var common = require('../translation/common');
 var match = require('../translation/match-ast');
-var list = match.list;
-var variable = match.variable;
 
 module.exports.translate = function translate (parsedExpression) {
-    return match(parsedExpression, [
-        list('+', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('+', translate, true, true),
-        list('*', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('*', translate, true, true),
-        list('-', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('-', translate, true, true),
-        list('/', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('/', translate, true, true),
-        list('+', variable('a'), variable('b', 'list')),            common.infixOperator('+', translate, false, true),
-        list('*', variable('a'), variable('b', 'list')),            common.infixOperator('*', translate, false, true),
-        list('-', variable('a'), variable('b', 'list')),            common.infixOperator('-', translate, false, true),
-        list('/', variable('a'), variable('b', 'list')),            common.infixOperator('/', translate, false, true),
-        list('+', variable('a', 'list'), variable('b')),            common.infixOperator('+', translate, true, false),
-        list('*', variable('a', 'list'), variable('b')),            common.infixOperator('*', translate, true, false),
-        list('-', variable('a', 'list'), variable('b')),            common.infixOperator('-', translate, true, false),
-        list('/', variable('a', 'list'), variable('b')),            common.infixOperator('/', translate, true, false),
-        list('+', variable('a'), variable('b')),                    common.infixOperator('+', translate),
-        list('*', variable('a'), variable('b')),                    common.infixOperator('*', translate),
-        list('-', variable('a'), variable('b')),                    common.infixOperator('-', translate),
-        list('/', variable('a'), variable('b')),                    common.infixOperator('/', translate),
-    ]);
+    return match(parsedExpression,
+        common.infixOperatorMatchersForLanguageWithInt32(translate)
+    );
 };
--- translation/common.js
+++ translation/common.js
@@ -1,5 +1,11 @@
 
-module.exports.infixOperator = function (operator, translate, nestFirst, nestSecond) {
+var common = module.exports;
+
+var match = require('../translation/match-ast');
+var list = match.list;
+var variable = match.variable;
+
+common.infixOperator = function (operator, translate, nestFirst, nestSecond) {
     return (variables) => {
         var first = translate(variables.get('a'));
         var second = translate(variables.get('b'));
@@ -9,4 +15,25 @@ module.exports.infixOperator = function (operator, translate, nestFirst, nestSec
 
         return first + ' ' + operator + ' ' + second;
     };
+};
+
+common.infixOperatorMatchersForLanguageWithInt32 = function (translate) {
+    return [
+        list('+', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('+', translate, true, true),
+        list('*', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('*', translate, true, true),
+        list('-', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('-', translate, true, true),
+        list('/', variable('a', 'list'), variable('b', 'list')),    common.infixOperator('/', translate, true, true),
+        list('+', variable('a'), variable('b', 'list')),            common.infixOperator('+', translate, false, true),
+        list('*', variable('a'), variable('b', 'list')),            common.infixOperator('*', translate, false, true),
+        list('-', variable('a'), variable('b', 'list')),            common.infixOperator('-', translate, false, true),
+        list('/', variable('a'), variable('b', 'list')),            common.infixOperator('/', translate, false, true),
+        list('+', variable('a', 'list'), variable('b')),            common.infixOperator('+', translate, true, false),
+        list('*', variable('a', 'list'), variable('b')),            common.infixOperator('*', translate, true, false),
+        list('-', variable('a', 'list'), variable('b')),            common.infixOperator('-', translate, true, false),
+        list('/', variable('a', 'list'), variable('b')),            common.infixOperator('/', translate, true, false),
+        list('+', variable('a'), variable('b')),                    common.infixOperator('+', translate),
+        list('*', variable('a'), variable('b')),                    common.infixOperator('*', translate),
+        list('-', variable('a'), variable('b')),                    common.infixOperator('-', translate),
+        list('/', variable('a'), variable('b')),                    common.infixOperator('/', translate),
+    ];
 };
