6e1b41319e1de8fb5a7ec926913778569e50db03
Jesper Oskarsson <jesosk@gmail.com>
2016-06-16 20:32:08 +0200

    Improved step definition error checking

--- features/step_definitions/common.js
+++ features/step_definitions/common.js
@@ -5,7 +5,7 @@ var Promise = require('promise');
 var exec = require('child_process').exec;
 var temp = require('temp').track();
 var fs = require('fs');
-require('should');
+var should = require('should');
 
 module.exports.stepDefinitions = function (language, suffix, programBuilder, commandBuilder) {
     var evaluateExpression = module.exports.compileAndEvaluateExpression(suffix, programBuilder, commandBuilder);
@@ -34,15 +34,18 @@ module.exports.stepDefinitions = function (language, suffix, programBuilder, com
         });
 
         this.Then(/^I should get a translation error "([^"]*)"$/, function (expected) {
+            should(this.translationError).not.be.null();
             this.translationError.should.be.instanceOf(Error);
             this.translationError.message.should.equal(expected);
         });
 
         this.Then(/^the error column should say (\d+)$/, function (expected) {
+            should(this.errorColumn).not.be.null();
             this.errorColumn.should.equal(parseInt(expected, 10));
         });
 
         this.Then(/^the error line should say (\d+)$/, function (expected) {
+            should(this.errorLine).not.be.null();
             this.errorLine.should.equal(parseInt(expected, 10));
         });
     };
