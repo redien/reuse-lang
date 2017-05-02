da053184813e955872f0b1fde220970a0c0bc3f6
Jesper Oskarsson <jesosk@gmail.com>
2017-05-02 21:38:01 +0200

    Remove previous build files as they were sometimes reused by the next build

--- bootstrap/eval.sh
+++ bootstrap/eval.sh
@@ -5,6 +5,8 @@ current_path=$(pwd)
 cd $(dirname "$0")
 
 echo "(define execute () $1)" > ../generated/test.lisp
+rm -R ../generated/test/src
+rm -R ../generated/test/lib
 build_output=$(node reuse.js ../generated/test.lisp test 2>&1 ; cd ../generated/test ; npm install 2>&1 ; npm run build 2>&1)
 node -e "console.log(require('../generated/test/lib/js/src/source.js').execute())"
 
