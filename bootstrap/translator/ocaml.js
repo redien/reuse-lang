0260084c8f708ea914931c82973bd767e2f6cc3c
Jesper Oskarsson <jesosk@gmail.com>
2017-05-02 21:38:36 +0200

    implemented definition translation

--- bootstrap/translator/ocaml.js
+++ bootstrap/translator/ocaml.js
@@ -1,9 +1,20 @@
 
 var ast = require('../parser/ast');
 
+var translateExpression = function (expression) {
+    return ast.value(expression);
+};
+
+var translateDefinition = function (definition) {
+    var name = ast.value(ast.child(definition, 1));
+    var expression = translateExpression(ast.child(definition, 3));
+
+    return 'let ' + name + ' = fun _ -> ' + expression + ';;';
+};
+
 module.exports.translate = function (expression) {
     return [
-        {filename: 'src/source.ml', contents: 'let execute = fun _ -> 1;;'},
+        {filename: 'src/source.ml', contents: translateDefinition(expression)},
         {filename: 'bsconfig.json', contents: '{"name" : "hello", "sources" : { "dir" : "src" }}'},
         {filename: 'package.json', contents: '{ "dependencies": { "bs-platform": "1.7.0" }, "scripts" : { "build" : "bsb", "watch" : "bsb -w" } }'}
     ];
