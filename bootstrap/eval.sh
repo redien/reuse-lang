0d93e12cf6ec6dfafdcb931cd20783bcc8aca637
Jesper Oskarsson <jesosk@gmail.com>
2017-05-15 22:49:39 +0200

    Fixed path in bootstrap eval

--- bootstrap/eval.sh
+++ bootstrap/eval.sh
@@ -6,4 +6,4 @@ mkdir -p $script_path/../generated/
 lisp_source=$script_path/../generated/boostrap-eval.lisp
 echo "$1 (export execute () $2)" > $lisp_source
 $script_path/compile-library.sh $lisp_source $script_path/../generated/boostrap-eval.js
-node -e "console.log(require('$script_path/../generated/boostrap-eval.js').execute())"
+node -e "console.log(require('./$script_path/../generated/boostrap-eval.js').execute())"
