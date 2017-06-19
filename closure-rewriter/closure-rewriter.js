12eadc575090c2960faf9d0662a653607e422479
Jesper Oskarsson <jesosk@gmail.com>
2017-06-19 23:45:12 +0200

    Started writing bootstrap implementation of closure rewriter

---
+++ closure-rewriter/closure-rewriter.js
@@ -0,0 +1,23 @@
+
+var fs = require('fs');
+var ast = require(__dirname + '/../bootstrap/parser/ast');
+var parser = require(__dirname + '/../bootstrap/parser/parser');
+
+function translateDefinition(definition) {
+    return definition;
+}
+
+function rewriteModule (definitions) {
+    return ast.map(definitions, translateDefinition);
+}
+
+function parse (source) {
+    var result = parser.parse(source);
+    return result.ast;
+}
+
+var input = fs.readFileSync(process.argv[2], 'utf8');
+var expressions = parse(input);
+var rewrittenExpression = rewriteModule(expressions);
+var output = ast.join(ast.map(rewrittenExpression, ast.toString), ' ');
+fs.writeFileSync(process.argv[3], output, 'utf8');
