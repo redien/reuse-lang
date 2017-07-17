db3461c28dc9ebcf52dc7a529946858a48a535b0
Jesper Oskarsson <jesosk@gmail.com>
2017-07-17 22:49:49 +0200

    Extended eval script to take input from stdin

--- extended/bootstrap/eval.sh
+++ extended/bootstrap/eval.sh
@@ -7,11 +7,23 @@ generated_folder=$script_path/../../generated/$random_name
 mkdir -p $generated_folder
 
 program_source=$generated_folder/program_source.lisp
-echo "$1 (export execute () $2)" > $program_source
 
-$script_path/compile-library.sh $program_source $generated_folder
-printf "\nPrintf.printf \"%%d\\\n\" (Int32.to_int (execute ()))\n" >> $generated_folder/ocaml/source.ml
-docker run --rm -v $PWD/$generated_folder/ocaml:/home/ocaml ocaml/opam:alpine-3.6_ocaml-4.06.0 ocaml ../ocaml/source.ml
+if [ "$3" == "--stdin" ]
+then
+    echo "$1 (export main (stdin) $2)" > $program_source
+    $script_path/compile-library.sh $program_source $generated_folder
+    printf "$(cat $script_path/stdin_wrapper.ml)\n$(cat $generated_folder/ocaml/source.ml)" > $generated_folder/ocaml/source.ml
+    printf "\nPrintf.printf \"%%s\\\n\" (_list_to_string (main _stdin_list))\n" >> $generated_folder/ocaml/source.ml
+    docker build $script_path -t reuse-ocaml >&2
+    docker run --rm -v $PWD/$generated_folder/ocaml:/home/ocaml reuse-ocaml ocamlfind ocamlc -linkpkg -thread -package core /home/ocaml/source.ml -o /home/ocaml/out
+    cat | docker run -i --rm -v $PWD/$generated_folder/ocaml:/home/ocaml reuse-ocaml /home/ocaml/out
+else
+    echo "$1 (export main () $2)" > $program_source
+    $script_path/compile-library.sh $program_source $generated_folder
+    printf "\nPrintf.printf \"%%d\\\n\" (Int32.to_int (main ()))\n" >> $generated_folder/ocaml/source.ml
+    docker run --rm -v $PWD/$generated_folder/ocaml:/home/ocaml ocaml/opam:alpine-3.6_ocaml-4.06.0 ocaml ../ocaml/source.ml
+fi
+
 result=$?
 
 rm -R $generated_folder
