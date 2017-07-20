5626aa0ced02272eead68ace8661745ee57a5cc9
Jesper Oskarsson <jesosk@gmail.com>
2017-07-20 23:06:13 +0200

    Lists from stdin should be int32 instead of int

--- extended/bootstrap/stdin_wrapper.ml
+++ extended/bootstrap/stdin_wrapper.ml
@@ -6,16 +6,16 @@ let _stdin_str = In_channel.input_all stdin;;
 let rec _string_to_list_i = fun input i result ->
     if i > 0 then
         let sub_input = (String.sub input 0 ((String.length input) - 1)) in
-            _string_to_list_i sub_input (i - 1) (CCons ((Char.code (String.get input i)), result))
+            _string_to_list_i sub_input (i - 1) (CCons ((Int32.of_int (Char.code (String.get input i))), result))
     else
-        CCons ((Char.code (String.get input i)), result);;
+        CCons ((Int32.of_int (Char.code (String.get input i))), result);;
 
 let _string_to_list = fun input -> _string_to_list_i input ((String.length input) - 1) CEmpty;;
 
 let rec _list_to_string_r = fun input result ->
     match input with
           CCons(x, rest) ->
-            let string_from_int = (String.make 1 (Char.chr x)) in
+            let string_from_int = (String.make 1 (Char.chr (Int32.to_int x))) in
             let new_result = (String.concat "" (result :: string_from_int :: [])) in
                 (_list_to_string_r rest new_result)
         | CEmpty -> result;;
