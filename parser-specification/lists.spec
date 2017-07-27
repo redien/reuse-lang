4a532bab158b0b4ac1b5fe9d28459f48d42ee594
Jesper Oskarsson <jesosk@gmail.com>
2017-07-27 21:56:59 +0200

    Parse empty list

---
+++ parser-specification/lists.spec
@@ -0,0 +1,16 @@
+
+Should parse an empty list
+> ()
+= ()
+
+Should parse a list in a list
+> (())
+= (())
+
+Should parse several lists inside a list
+> (() () ())
+= (() () ())
+
+Should ignore whitespace inside an empty list
+> ( )
+= ()
--- parser/parser.clj
+++ parser/parser.clj
@@ -1,5 +1,25 @@
 
+(def reverse' (list new-list)
+     (match list
+            Empty       new-list
+            (Cons x xs) (reverse' xs (Cons x new-list))))
+
+(def reverse (list)
+     (reverse' list Empty))
+
+(def foldr (f initial list)
+     (match list
+            Empty       initial
+            (Cons x xs) (f x (foldr f initial xs))))
+
+(def map (f list)
+     (foldr (fn (x xs)
+                (Cons (f x) xs))
+            Empty
+            list))
+
 (data boolean True False)
+(data (pair a b) (Pair a b))
 
 (def not (a)
      (match a
@@ -25,16 +45,61 @@
 (def = (a b)
      (not (or (< a b) (> a b))))
 
-(def skip-while (f input)
+(def read-while' (predicate input string)
      (match input
-            Empty       input
-            (Cons x xs) (match (f x)
-                               True  (skip-while f xs)
-                               False input)))
+            Empty       (Pair input string)
+            (Cons x xs) (match (predicate x)
+                               True  (read-while' predicate xs (Cons x string))
+                               False (Pair input string))))
+
+(def read-while (predicate input)
+     (match (read-while' predicate input Empty)
+            (Pair input Empty)  (Pair input Empty)
+            (Pair input string) (Pair input (reverse string))))
+
+(def skip-while (predicate input)
+     (match (read-while' predicate input Empty)
+            (Pair input _) input))
 
 (def whitespace? (character)
      (= character 32))
 
-(def parse (input) (skip-while whitespace? input))
+(def not-whitespace? (character)
+     (not (whitespace? character)))
 
-(def stringify (input) input)
+(data expression (Symbol (list int32))
+                 (List (list expression)))
+
+(def read-symbol (input)
+     (match (read-while not-whitespace? input)
+            (Pair input name) (Pair input (Symbol name))))
+
+(def parse' (input)
+     (match input
+            Empty       (Pair input (List Empty))
+            (Cons x xs) (match (= x 40)
+                               True (Pair input (List Empty))
+                               False (read-symbol input))))
+
+(def parse (input)
+     (match (parse' (skip-while whitespace? input))
+            (Pair _ expression) expression))
+
+(def str (character)
+     (Cons character Empty))
+
+(def concat-strings (a b)
+     (match a
+            Empty       b
+            (Cons x xs) (concat-strings xs (Cons x b))))
+
+(def join (separator list)
+     (foldr (fn (x xs)
+                (concat-strings x (concat-strings separator xs)))
+            Empty
+            list))
+
+(def stringify (input)
+     (match input
+            (Symbol name)      name
+            (List expressions) (concat-strings (str 40) (concat-strings (join (str 32) (map stringify expressions)) (str 41)))))
