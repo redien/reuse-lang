a98c49fb9da9dd6847e942e796159acb436516a0
Jesper Oskarsson <jesosk@gmail.com>
2017-12-06 04:12:35 +0100

    Added missing export in interpreter input

--- extended/interpreter/eval.sh
+++ extended/interpreter/eval.sh
@@ -3,7 +3,7 @@
 script_path=$(dirname "$0")
 project_root="$script_path/../.."
 
-echo "$1 (main () $2)" | "$project_root/extended/interpreter/bootstrap/eval.sh" "$(cat $project_root/generated/extended/interpreter/interpreter.clj)" "(eval (parse stdin))" --stdin
+echo "$1 (export main () $2)" | "$project_root/extended/interpreter/bootstrap/eval.sh" "$(cat $project_root/generated/extended/interpreter/interpreter.clj)" "(eval (parse stdin))" --stdin
 
 result=$?
 
