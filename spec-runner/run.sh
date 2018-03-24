02da7804a665a75bbbf0068ed7fcb8e9f219dff6
Jesper Oskarsson <jesosk@gmail.com>
2018-03-24 14:09:06 +0100

    Passing user to docker container makes no sense

--- spec-runner/run.sh
+++ spec-runner/run.sh
@@ -3,5 +3,5 @@
 script_path=$(dirname "$0")
 
 docker build $script_path -t bavpj9kxxekvg
-docker run --rm --user $USER -v $PWD/$script_path/../:/home/build/ -w /home/build/ --entrypoint /bin/bash bavpj9kxxekvg ./spec-runner/spec-runner.sh "$1" "$2"
+docker run --rm -v $PWD/$script_path/../:/home/build/ -w /home/build/ --entrypoint /bin/bash bavpj9kxxekvg ./spec-runner/spec-runner.sh "$1" "$2"
 
