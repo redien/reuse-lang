7575b03e62f064daf1b8a903bd7a0090518baab2
Jesper Oskarsson <jesosk@gmail.com>
2018-04-25 20:30:14 +0000

    Put dependencies into dev-env directory instead of embedding it in the spec runner.

--- spec-runner/run.sh
+++ spec-runner/run.sh
@@ -1,7 +1,67 @@
 #!/bin/bash
 
-script_path=$(dirname "$0")
+spec=$1
+eval_command=$2
 
-docker build $script_path -t bavpj9kxxekvg
-docker run --rm -v $PWD/$script_path/../:/home/build/ -w /home/build/ --entrypoint /bin/bash bavpj9kxxekvg ./spec-runner/spec-runner.sh "$1" "$2"
+tests=0
+passing=0
+failing=0
 
+mkdir -p generated
+rm generated/build.log > /dev/null 2>&1
+
+function testLine {
+    result=$($eval_command "${2}" "${1:2}" 2>> generated/build.log)
+
+    if [ "$?" != "0" ]; then
+        result="Build error"
+    fi
+
+    tests=$((tests+1))
+
+    if [ "$result" == "${3:2}" ]; then
+        echo ok $tests - ${1:2} = $result
+        passing=$((passing+1))
+    else
+        echo not ok $tests - expected \"${3:2}\" but got \"$result\"
+        echo  ---
+        echo    expression: \'${1:2}\'
+        echo    program: \'${2}\'
+        echo    expected: \'${3:2}\'
+        echo    actual:   \'$result\'
+        echo  ...
+        failing=$((failing+1))
+    fi
+}
+
+echo TAP version 13
+
+program=""
+
+for f in $spec/*.spec
+do
+    while read line; do
+        firstChar=${line:0:1}
+        if [ "$firstChar" == ">" ]; then
+            read expected
+            testLine "$line" "$program" "$expected"
+            program=""
+        elif [ "$firstChar" == "|" ]; then
+            program=$program${line:2}
+        else
+            if [ "$line" != "" ]; then
+                echo \# $line
+            fi
+        fi
+    done <$f
+done
+
+echo 1..$tests
+
+echo \# tests $tests
+echo \# pass  $passing
+echo \# fail  $failing
+
+if [ "$failing" != "0" ]; then
+    exit 1
+fi
