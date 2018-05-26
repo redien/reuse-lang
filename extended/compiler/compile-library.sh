0986877b867f0df493fbb48d759046a9e149f06f
Jesper Oskarsson <jesosk@gmail.com>
2018-05-26 15:10:00 +0200

    Let reuse compiler make use of ocaml bootstrap framework

---
+++ extended/compiler/compile-library.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+script_path=$(dirname "$0")
+project_root=$script_path/../..
+compiler_source=$project_root/generated/extended/compiler/compiler.clj
+
+echo >&2
+
+echo Source: >&2
+cat "$1" >&2
+echo >&2
+
+echo OCaml: >&2
+mkdir -p $2/ocaml
+echo "$1" | "$project_root/extended/interpreter/bootstrap/eval.sh" "$(cat $project_root/generated/extended/compiler/compiler.clj)" "(to-ocaml (sexps-to-definitions (parse stdin)))" --stdin > $2/ocaml/source.ml
+result=$?
+cat $2/ocaml/source.ml >&2
+echo >&2
+
+exit $result
--- extended/compiler/eval.sh
+++ extended/compiler/eval.sh
@@ -1,11 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-script_path=$(dirname "$0")
-project_root="$script_path/../.."
-
-echo "$1 (export main () $2)" | "$project_root/extended/interpreter/bootstrap/eval.sh" "$(cat $project_root/generated/extended/compiler/compiler.clj)" "(to-ocaml (sexps-to-definitions (parse stdin)))" --stdin
-
-result=$?
-
-exit $result
-
+IMPL=compiler `dirname $0`/../eval.sh "$1" "$2" "$3"
