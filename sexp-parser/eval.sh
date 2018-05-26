bb62f84041b9ccad9f77b80fc96e3faadf9e1379
Jesper Oskarsson <jesosk@gmail.com>
2018-05-26 16:49:43 +0200

    sexp-parser shouldn't include program from specification

--- sexp-parser/eval.sh
+++ sexp-parser/eval.sh
@@ -2,4 +2,4 @@
 
 script_path=$(dirname "$0")
 
-echo "$2" | $script_path/../extended/interpreter/bootstrap/eval.sh "$(cat $script_path/../generated/sexp-parser/parser.clj)$1" "(stringify (parse stdin))" --stdin
+echo "$2" | $script_path/../extended/interpreter/bootstrap/eval.sh "$(cat $script_path/../generated/sexp-parser/parser.clj)" "(stringify (parse stdin))" --stdin
