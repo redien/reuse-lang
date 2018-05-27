6836f46fd5f80c328ced0e89b70f89b6a54f5346
Jesper Oskarsson <jesosk@gmail.com>
2018-05-27 02:59:18 +0200

    Do not say we're printing the source when compilation didn't succeed

--- extended/compiler/compile-library.sh
+++ extended/compiler/compile-library.sh
@@ -12,11 +12,9 @@ echo Source: >&2
 cat "$1" >&2
 echo >&2
 
-echo OCaml: >&2
 mkdir -p $2/ocaml
 cat "$1" | "$project_root/extended/interpreter/bootstrap/eval.sh" "$(cat $project_root/generated/extended/compiler/compiler.clj)" "(to-ocaml (sexps-to-definitions (parse stdin)))" --stdin > $2/ocaml/source.ml
-result=$?
+
+echo OCaml: >&2
 cat $2/ocaml/source.ml >&2
 echo >&2
-
-exit $result
