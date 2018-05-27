bba40b161a8b1b324554482b7456dea55dc96719
Jesper Oskarsson <jesosk@gmail.com>
2018-05-27 02:58:46 +0200

    Trap errors in extended eval.sh and cleanup

--- extended/eval.sh
+++ extended/eval.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+
 script_path=$(dirname "$0")
 
 random_name=$(node -e "console.log(crypto.randomBytes(Math.ceil(12)).toString('hex').slice(0,12))")
 generated_folder=$script_path/../generated/$random_name
 mkdir -p $generated_folder
+cleanup() {
+    rm -R $generated_folder
+}
+trap cleanup EXIT
 
 program_source=$generated_folder/program_source.lisp
 
@@ -23,7 +29,5 @@ else
 fi
 
 result=$?
-
-rm -R $generated_folder
 
 exit $result
