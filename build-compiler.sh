318bf3ae114175a4a8f06ffd8638a79e1fad9547
Jesper Oskarsson <jesosk@gmail.com>
2018-06-04 22:18:18 +0200

    First attempt at building Reuse -> OCaml compiler

---
+++ build-compiler.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+script_path=$(dirname $0)
+output_dir=$script_path/extended/compiler
+temporary_dir=$(mktemp -d)
+
+$script_path/build.sh
+$script_path/extended/bootstrap/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
+printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (fn_stringify (fn_definitions_45to_45sexps (to_ocaml (fn_sexps_45to_45definitions (fn_parse _stdin_list))))))\n" >> $temporary_dir/ocaml/source.ml
+cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
