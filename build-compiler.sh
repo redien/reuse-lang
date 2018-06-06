b63bacc36ffc05745ef3c6e86573822b94a5f9bd
Jesper Oskarsson <jesosk@gmail.com>
2018-06-07 01:09:04 +0200

    Add debug information to compiler build

--- build-compiler.sh
+++ build-compiler.sh
@@ -8,4 +8,4 @@ $script_path/build.sh
 $script_path/extended/bootstrap/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
 printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (fn_sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
 cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
-ocamlc $output_dir/compiler.ml -o $output_dir/compiler.out
+ocamlc -g $output_dir/compiler.ml -o $output_dir/compiler.out
