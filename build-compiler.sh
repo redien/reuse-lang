55e29bcd8e204072aea8d8d5abb5fad0e678e6a8
Jesper Oskarsson <jesosk@gmail.com>
2018-06-08 16:55:07 +0000

    Do not save compiler before it successfully builds

--- build-compiler.sh
+++ build-compiler.sh
@@ -2,10 +2,14 @@
 
 script_path=$(dirname $0)
 output_dir=$script_path/extended/compiler
-temporary_dir=$(mktemp -d)
+temporary_dir=$(mktemp -d -p $script_path/generated)
+
+set -e
 
 $script_path/build.sh
 $script_path/extended/bootstrap/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
 printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (fn_sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
+ocamlc -g $temporary_dir/ocaml/source.ml -o $temporary_dir/compiler.out
 cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
-ocamlc -g $output_dir/compiler.ml -o $output_dir/compiler.out
+cp $temporary_dir/compiler.out $output_dir/compiler.out
+
