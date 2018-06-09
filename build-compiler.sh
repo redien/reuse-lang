8d6a22f7869dbc27aba4d768105b093ac2c363c1
Jesper Oskarsson <jesosk@gmail.com>
2018-06-09 08:23:00 +0200

    Compiler bootstraps :)

--- build-compiler.sh
+++ build-compiler.sh
@@ -7,8 +7,8 @@ temporary_dir=$(mktemp -d -p $script_path/generated)
 set -e
 
 $script_path/build.sh
-$script_path/extended/bootstrap/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
-printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (fn_sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
+$script_path/extended/compiler/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
+printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
 ocamlc -g $temporary_dir/ocaml/source.ml -o $temporary_dir/compiler.out
 cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
 cp $temporary_dir/compiler.out $output_dir/compiler.out
