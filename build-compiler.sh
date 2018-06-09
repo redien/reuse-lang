6eb80eb1ae7626d332773d9e6406ba5217a6bfac
Jesper Oskarsson <jesosk@gmail.com>
2018-06-09 08:37:10 +0200

    Do not automatically promote compiled compiler

--- build-compiler.sh
+++ build-compiler.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 script_path=$(dirname $0)
-output_dir=$script_path/extended/compiler
+output_dir=$script_path/generated/extended/compiler
 temporary_dir=$(mktemp -d -p $script_path/generated)
 
+mkdir -p $script_path/bin
+
 set -e
+
+ocamlc -g $script_path/extended/compiler/compiler.ml -o $script_path/bin/reuse-ocaml
 
 $script_path/build.sh
 $script_path/extended/compiler/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
 printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
 ocamlc -g $temporary_dir/ocaml/source.ml -o $temporary_dir/compiler.out
 cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
-cp $temporary_dir/compiler.out $output_dir/compiler.out
-
--- extended/compiler/compile-library.sh
+++ extended/compiler/compile-library.sh
@@ -7,4 +7,4 @@ project_root=$script_path/../..
 compiler_source=$project_root/generated/extended/compiler/compiler.reuse
 
 mkdir -p $2/ocaml
-cat "$1" | "$project_root/extended/compiler/compiler.out" > $2/ocaml/source.ml
+cat "$1" | "$project_root/bin/reuse-ocaml" > $2/ocaml/source.ml
