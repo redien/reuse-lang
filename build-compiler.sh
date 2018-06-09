b15bd2b2c4234f3de676e27e686634222c709301
Jesper Oskarsson <jesosk@gmail.com>
2018-06-09 09:06:03 +0200

    Made built/current compiler separate from each other

--- build-compiler.sh
+++ build-compiler.sh
@@ -11,7 +11,17 @@ set -e
 ocamlc -g $script_path/extended/compiler/compiler.ml -o $script_path/bin/reuse-ocaml
 
 $script_path/build.sh
-$script_path/extended/compiler/compile-library.sh "$script_path/generated/extended/compiler/compiler.reuse" "$temporary_dir"
-printf "\n$(cat $script_path/extended/stdin_wrapper.ml)\nPrintf.printf \"%%s\" (_list_to_string (to_ocaml (sexps_45to_45definitions (parse _stdin_list))))\n" >> $temporary_dir/ocaml/source.ml
+mkdir -p $temporary_dir/ocaml
+cat $script_path/generated/extended/compiler/compiler.reuse | $script_path/bin/reuse-ocaml > $temporary_dir/ocaml/source.ml
+
+cat << END_OF_SOURCE >> $temporary_dir/ocaml/source.ml
+
+$(cat $script_path/extended/stdin_wrapper.ml)
+Printf.printf "%s" (_list_to_string (to_ocaml (sexps_45to_45definitions (parse _stdin_list))))
+
+END_OF_SOURCE
+
 ocamlc -g $temporary_dir/ocaml/source.ml -o $temporary_dir/compiler.out
+
 cp $temporary_dir/ocaml/source.ml $output_dir/compiler.ml
+cp $temporary_dir/compiler.out $output_dir/reuse-ocaml
