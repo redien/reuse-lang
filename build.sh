f2df0f70c6eb07f2000a45445d527a74f96bc5e5
Jesper Oskarsson <jesosk@gmail.com>
2018-06-22 22:02:16 +0200

    Split strings from rest of parser

--- build.sh
+++ build.sh
@@ -28,7 +28,6 @@ $(cat $standard_library/result.reuse)"
 echo "$standard_library_compiled" > $project_root/generated/standard-library.reuse
 
 echo "$standard_library_compiled $(cat $project_root/sexp-parser/parser.reuse)" > $project_root/generated/sexp-parser/parser.reuse
-echo "$standard_library_compiled $(cat $project_root/sexp-parser/parser.reuse)$(cat $project_root/parser/parser.reuse)" > $project_root/generated/parser/parser.reuse
+echo "$standard_library_compiled $(cat $project_root/sexp-parser/parser.reuse)$(cat $project_root/parser/strings.reuse)$(cat $project_root/parser/parser.reuse)" > $project_root/generated/parser/parser.reuse
 echo "$(cat $project_root/generated/parser/parser.reuse)$(cat $project_root/extended/interpreter/interpreter.reuse)" > $project_root/generated/extended/interpreter/interpreter.reuse
 echo "$(cat $project_root/generated/parser/parser.reuse)$(cat $project_root/extended/compiler/compiler.reuse)" > $project_root/generated/extended/compiler/compiler.reuse
-
