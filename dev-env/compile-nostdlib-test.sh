28096f0a7de2be6455af4aca4fb539dcfe8d7ba3
Jesper Oskarsson <jesosk@gmail.com>
2019-01-12 14:24:16 +0100

    Change the format string in nostdlib tests to directly accept 32 bit integers

--- dev-env/compile-nostdlib-test.sh
+++ dev-env/compile-nostdlib-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-printf "$(cat $1)\nPrintf.printf \"%%d\" (Int32.to_int (main ()))\n" > "$1.2.ml"
+printf "$(cat $1)\nPrintf.printf \"%%ld\" (main ())\n" > "$1.2.ml"
 ocamlc -g "$1.2.ml" -o "$1.out"
 rm "$1.2.ml"
 chmod +x "$1.out"
