5762febe169862c4cddd521f8774f14a184fdf33
Jesper Oskarsson <jesosk@gmail.com>
2019-01-23 22:24:00 +0100

    Removed tap-spec as a dependency

---
+++ dev-env/tap-format/tap-format.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+title_format="  \033[4m"
+ok_format="    \033[32m✔\033[90m "
+not_ok_format="    \033[91m✖ "
+result_failed_format="\033[91m "
+result_passed_format="\033[92m "
+reset_format="\033[0m"
+failed_tests=0
+
+while IFS= read -r line; do
+    if [[ "$line" == "# fail "* ]]; then
+        echo -e "$result_failed_format${line:7} tests failed$reset_format\n\n"
+    elif [[ "$line" == "# pass "* ]]; then
+        echo -e "$result_passed_format${line:7} tests passed$reset_format"
+    elif [[ "$line" == "# tests "* ]]; then
+        echo -e "\n\n ${line:8} tests in total$reset_format"
+    elif [[ "$line" == "# "* ]]; then
+        echo -e "\n$title_format${line:2}$reset_format\n"
+    elif [[ "$line" == "ok "* ]]; then
+        echo -e "$ok_format${line:2}$reset_format"
+    elif [[ "$line" == "not ok "* ]]; then
+        echo -e "$not_ok_format${line:6}$reset_format"
+        failed_tests=1
+    fi
+done
+
+if [[ $failed_tests == 1 ]]; then
+    exit 1
+fi
