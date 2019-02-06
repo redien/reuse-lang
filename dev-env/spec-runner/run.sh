2d6af0048778b759b4ec515057ebb2d33e6d3c23
Jesper Oskarsson <jesosk@gmail.com>
2019-02-06 21:29:07 +0100

    Fixed spec runner to align yaml values

--- dev-env/spec-runner/run.sh
+++ dev-env/spec-runner/run.sh
@@ -21,51 +21,51 @@ function testLine {
 
     if [ "${3:0:1}" == "=" ]; then
         if [ "$status_code" != "0" ]; then
-            echo not ok $tests - program returned non-zero status code
-            echo  ---
-            echo    expression:  \'${1:2}\'
-            echo    program:     \'${2}\'
-            echo    status-code: \'$status_code\'
-            echo    error:
+            echo "not ok $tests - program returned non-zero status code"
+            echo "---"
+            echo "  expression:  '${1:2}'"
+            echo "  program:     '${2}'"
+            echo "  status-code: '$status_code'"
+            echo "  error:"
             echo "$(cat generated/error.log)"
-            echo  ...
+            echo "..."
             failing=$((failing+1))
 
         elif [ "$result" == "${3:2}" ]; then
-            echo ok $tests - ${1:2} = $result
+            echo "ok $tests - ${1:2} = $result"
             passing=$((passing+1))
 
         else
-            echo not ok $tests - expected \'${3:2}\' but got \'$result\'
-            echo  ---
-            echo    expression: \'${1:2}\'
-            echo    program:    \'${2}\'
-            echo    expected:   \'${3:2}\'
-            echo    actual:     \'$result\'
-            echo  ...
+            echo "not ok $tests - expected '${3:2}' but got '$result'"
+            echo "---"
+            echo "  expression: '${1:2}'"
+            echo "  program:    '${2}'"
+            echo "  expected:   '${3:2}'"
+            echo "  actual:     '$result'"
+            echo "..."
             failing=$((failing+1))
         fi
     
     else
         if [ "$status_code" == "0" ]; then
-            echo not ok $tests - expected program to return a non-zero status code
-            echo  ---
-            echo    status-code:   \'$status_code\'
-            echo  ...
+            echo "not ok $tests - expected program to return a non-zero status code"
+            echo "---"
+            echo "  status-code:   '$status_code'"
+            echo "..."
             failing=$((failing+1))
 
         elif [[ "$result" == *"${3:2}"* ]]; then
-            echo ok $tests - Error contains \'${3:2}\'
+            echo "ok $tests - Error contains '${3:2}'"
             passing=$((passing+1))
 
         else
-            echo not ok $tests - expected \'$result\' to contain \'${3:2}\'
-            echo  ---
-            echo    expression: \'${1:2}\'
-            echo    program:    \'${2}\'
-            echo    expected:   \'${3:2}\'
-            echo    actual:     \'$result\'
-            echo  ...
+            echo "not ok $tests - expected '$result' to contain '${3:2}'"
+            echo "---"
+            echo "  expression: '${1:2}'"
+            echo "  program:    '${2}'"
+            echo "  expected:   '${3:2}'"
+            echo "  actual:     '$result'"
+            echo "..."
             failing=$((failing+1))
         fi
     fi
