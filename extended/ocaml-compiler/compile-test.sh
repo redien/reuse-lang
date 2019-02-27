03aa9cc39498dc9d4df8eb8968eae684321c6548
Jesper Oskarsson <jesosk@gmail.com>
2019-02-27 19:24:14 +0100

    Added file path to reuse compiler error message

--- extended/ocaml-compiler/compile-test.sh
+++ extended/ocaml-compiler/compile-test.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-set -e
 
 script_path=$(dirname "$0")
 project_root=$script_path/../..
 
 cat "$1" | "$project_root/generated/extended/compiler-ocaml" > "$2/executable.ml"
+result=$?
+if [ "$result" != "0" ]; then
+    >&2 printf "\n in file $1\n"
+    exit $result
+fi
+
+set -e
 $project_root/dev-env/compile-nostdlib-test.sh "$2/executable.ml"
