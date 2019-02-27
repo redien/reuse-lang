074e0cb870b4669d2324267e79b9bfe63d50bc0a
Jesper Oskarsson <jesosk@gmail.com>
2019-02-27 19:23:29 +0100

    Update repl.sh

--- dev-env/repl.sh
+++ dev-env/repl.sh
@@ -1,30 +1,61 @@
 #!/usr/bin/env bash
 
 project_root=$(dirname $0)/..
-
-$project_root/standard-library/build.sh
-
 generated_source=$project_root/generated/repl.reuse
 
+while [[ $# -gt 0 ]]
+do
+    arg="$1"
+
+    case $arg in
+        --nostdlib)
+            nostdlib=true
+            shift
+        ;;
+        --*)
+            throw_error Unrecognized flag $arg
+        ;;
+        *)
+            additional_sources="$additional_sources $arg"
+            shift
+        ;;
+    esac
+done
+
+if [ "$nostdlib" != "true" ]; then
+    $project_root/standard-library/build.sh
+    additional_sources="$project_root/generated/standard-library.reuse $additional_sources"
+fi
+
+cleanup() {
+    rm $generated_source{,.ml,.ml.out,.ml.2.cmi,.ml.2.cmo} 2>/dev/null
+}
+
 eval_reuse() {
-    echo "$1 (export main (_) $2)" > $generated_source
+    echo "(export main (_) $1)" > $generated_source
 
     $project_root/reusec --language ocaml\
                          --output $generated_source.ml\
                          --nostdlib\
-                         $project_root/generated/standard-library.reuse\
+                         $additional_sources\
                          $generated_source
+
+    if [ "$?" != "0" ]; then
+        echo
+        cleanup
+        return
+    fi
 
     $project_root/dev-env/compile-stdin-test.sh $generated_source.ml
     if [ -e "$generated_source.ml.out" ]; then
         echo "" | $generated_source.ml.out
         printf "\n"
     fi
-    rm $generated_source{,.ml,.ml.out,.ml.2.cmi,.ml.2.cmo} 2>/dev/null
+    cleanup
 }
 
 printf "Reuse REPL\n"
 while read -p "> " -e line; do
     history -s "$line"
-    eval_reuse "" "$line"
+    eval_reuse "$line"
 done
