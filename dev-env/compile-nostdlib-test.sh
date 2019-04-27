dbabc42b4c9eb4366518a3306e1360a3826f5c8f
Jesper Oskarsson <jesosk@gmail.com>
2019-04-28 00:39:51 +0200

    entrypoint for tests needs to be unique

--- dev-env/compile-nostdlib-test.sh
+++ dev-env/compile-nostdlib-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-printf "$(cat $1)\nPrintf.printf \"%%ld\" (main ())\n" > "$1.2.ml"
+printf "$(cat $1)\nPrintf.printf \"%%ld\" (reuse_45main ())\n" > "$1.2.ml"
 ocamlc "$1.2.ml" -o "$1.out"
 rm "$1.2.ml"
 chmod +x "$1.out"
--- extended/eval.sh
+++ extended/eval.sh
@@ -7,7 +7,7 @@ mkdir -p $script_path/../generated/tests
 generated_folder=$(mktemp -d -p $script_path/../generated/tests)
 test_source=$generated_folder/test_source.reuse
 
-echo "$1 (def main () $2)" > $test_source
+echo "$1 (def reuse-main () $2)" > $test_source
 $script_path/$IMPL/compile-test.sh $test_source $generated_folder
 ./$generated_folder/executable.ml.out
 
