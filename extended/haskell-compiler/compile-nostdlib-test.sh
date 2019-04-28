457b22f6759bca481af19d82f6ad1803635c45b4
Jesper Oskarsson <jesosk@gmail.com>
2019-04-28 02:23:40 +0200

    Down to 10 failing tests

--- extended/haskell-compiler/compile-nostdlib-test.sh
+++ extended/haskell-compiler/compile-nostdlib-test.sh
@@ -2,6 +2,7 @@
 set -e
 
 printf "$(cat $1)\nmain = putStrLn (show reuse_45main)\n" > "$1.2.hs"
-ghc -v0 "$1.2.hs" -o "$2"
+echo "#!/usr/bin/env runghc" > "$2"
+cat "$1.2.hs" >> "$2"
 rm "$1.2.hs"
 chmod +x "$2"
