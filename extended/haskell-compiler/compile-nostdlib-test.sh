bd182a871e0b455cac4eecc537b9398134dc99e8
Jesper Oskarsson <jesosk@gmail.com>
2019-05-01 21:40:35 +0200

    Moved import and language declarations into compiler

--- extended/haskell-compiler/compile-nostdlib-test.sh
+++ extended/haskell-compiler/compile-nostdlib-test.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-printf '{-# LANGUAGE ExistentialQuantification #-}\nimport Data.Int\n' > "$1.2.hs"
-cat "$1" >> "$1.2.hs"
-printf '\nmain = putStrLn (show reuse_45main)\n' >> "$1.2.hs"
-echo "#!/usr/bin/env runghc" > "$2"
-cat "$1.2.hs" >> "$2"
-rm "$1.2.hs"
+printf '#!/usr/bin/env runghc\n' > "$2"
+cat "$1" >> "$2"
+printf '\nmain = putStrLn (show reuse_45main)\n' >> "$2"
 chmod +x "$2"
