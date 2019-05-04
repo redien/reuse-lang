af36e5d153c509c9d5f88a8d667c8e3e5d19cd2a
Jesper Oskarsson <jesosk@gmail.com>
2019-05-04 22:07:35 +0200

    Implemented StdinWrapper.hs

---
+++ extended/haskell-compiler/StdinWrapper.hs
@@ -0,0 +1,34 @@
+module StdinWrapper where
+
+import Data.Maybe
+import Data.Int
+import Data.List
+import Data.Char
+import CompilerHaskell
+
+stdin_string = getContents
+
+int32_to_char :: Int32 -> Char
+int32_to_char i =
+        chr (fromInteger (toInteger i))
+
+string_cons x xs =
+        concat [xs, [int32_to_char x]]
+
+char_to_int32 :: Char -> Int32
+char_to_int32 c =
+        fromInteger (toInteger (fromEnum c))
+
+string_to_reuse_string s = 
+        foldl (\s c -> string_45append (char_to_int32 c) s) string_45empty s
+
+stdin_get :: (Int32 -> a) -> a -> String -> Int32 -> a
+stdin_get succ fail s index =
+        let i = fromInteger (toInteger index) in
+        if i < (length s) && i >= 0 then
+                succ (char_to_int32 (s !! i))
+        else
+                fail
+
+list_to_string = string_45foldl string_cons ""
+stdin_list = (indexed_45iterator_45from stdin_string (stdin_get (\x -> CSome x) CNone))
