cc2fe4730c6ea00f54bbe80cedacf5f02e79cf60
Jesper Oskarsson <jesosk@gmail.com>
2019-05-05 16:31:54 +0200

    Haskell: Strictly evaluate arguments in StdinWrapper

--- extended/haskell-compiler/StdinWrapper.hs
+++ extended/haskell-compiler/StdinWrapper.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns #-}
 module StdinWrapper where
 
 import Data.Maybe
@@ -13,22 +14,22 @@ read_stdin = do
         evaluate $ force input
 
 int32_to_char :: Int32 -> Char
-int32_to_char i =
+int32_to_char !i =
         chr (fromInteger (toInteger i))
 
-string_cons x xs =
+string_cons !x !xs =
         concat [xs, [int32_to_char x]]
 
 char_to_int32 :: Char -> Int32
-char_to_int32 c =
+char_to_int32 !c =
         fromInteger (toInteger (fromEnum c))
 
-string_to_reuse_string s =
-        foldl (\s c -> string_45append (char_to_int32 c) s) string_45empty s
+string_to_reuse_string !s =
+        foldl (\ !s !c -> string_45append (char_to_int32 c) s) string_45empty s
 
 stdin_get :: (Int32 -> a) -> a -> String -> Int32 -> a
-stdin_get succ fail s index =
-        let i = fromInteger (toInteger index) in
+stdin_get !succ !fail !s !index =
+        let !i = fromInteger (toInteger index) in
         if i < (length s) && i >= 0 then
                 succ (char_to_int32 (s !! i))
         else
@@ -37,4 +38,4 @@ stdin_get succ fail s index =
 list_to_string = string_45foldl string_cons ""
 stdin_list = do
         stdin_string <- read_stdin
-        return (indexed_45iterator_45from stdin_string (stdin_get (\x -> CSome x) CNone))
+        return (indexed_45iterator_45from stdin_string (stdin_get (\ !x -> CSome x) CNone))
