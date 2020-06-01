7b9d3238a5390c0fd0a17f0730788497df3d4592
Jesper Oskarsson <jesosk@gmail.com>
2020-06-01 15:07:01 +0200

    Entrypoint is not needed in dockerfile

--- dev-env/develop.sh
+++ dev-env/develop.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-docker run --rm -it -v"$PWD":/home/opam/reuse-lang redien/reuse-lang-dev-env
-
+docker run --rm -it -v"$PWD":/home/opam/reuse-lang redien/reuse-lang-dev-env bash
