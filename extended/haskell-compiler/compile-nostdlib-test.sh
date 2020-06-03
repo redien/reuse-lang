11504c2f09ee98f97e0e065d6cc6b9d17da1de7d
Jesper Oskarsson <jesosk@gmail.com>
2020-06-03 22:20:17 +0200

    Use echo instead of printf for nodstdlib-test

--- extended/haskell-compiler/compile-nostdlib-test.sh
+++ extended/haskell-compiler/compile-nostdlib-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-printf '#!/usr/bin/env runghc\n' > "$2"
+echo -e '#!/usr/bin/env runghc\n' > "$2"
 cat "$1" >> "$2"
-printf '\nmain = putStrLn (show reuse_45main)\n' >> "$2"
+echo -e '\nmain = putStrLn (show reuse_45main)\n' >> "$2"
 chmod +x "$2"
