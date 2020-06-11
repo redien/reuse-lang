6e47163794e1fc5a66250a41541dcc4b60f3938b
Jesper Oskarsson <jesosk@gmail.com>
2020-06-11 22:16:37 +0200

    Simplified cli.ml

--- compiler-frontend/Cli.ml
+++ compiler-frontend/Cli.ml
@@ -1,7 +1,7 @@
 
 open ReuseCompiler;;
 
-let data_path = (Filename.dirname Sys.executable_name) ^ Filename.dir_sep ^ "data" ^ Filename.dir_sep;;
+let data_path = ml_string_to_reuse ((Filename.dirname Sys.executable_name) ^ Filename.dir_sep ^ "data" ^ Filename.dir_sep);;
 
 let argv = ml_list_to_reuse (List.map ml_string_to_reuse (List.tl (Array.to_list Sys.argv)));;
 
@@ -13,7 +13,7 @@ let read_file m path =
 let read_files m file_paths = list_45map (read_file m) file_paths;;
 let read_modules modules = let read_module m = (match m with CPair (module_name, path) -> read_file (CModulePath (module_name)) path)
                             in list_45map read_module modules;;
-let with_preamble files = list_45cons (read_file CModuleSelf (ml_string_to_reuse (data_path ^ reuse_string_to_ml (data_45preamble_45filename ())))) files;;
+let with_preamble files = list_45cons (read_file CModuleSelf (string_45concat data_path (data_45preamble_45filename ()))) files;;
 
 let write_file path content =
     let channel = open_out (reuse_string_to_ml path) in
@@ -24,7 +24,7 @@ let write_files files = ignore (list_45map (fun p -> match p with CPair (path, c
 let current = ref (CEventArguments argv);;
 
 while true do
-    match (on_45event (ml_string_to_reuse data_path) !current) with
+    match (on_45event data_path !current) with
           CCommandError (error) -> Printf.eprintf "%s\n" (reuse_string_to_ml error) ; exit 1
         | CCommandOutput (output) -> Printf.printf "%s" (reuse_string_to_ml output) ; exit 0
         | CCommandWriteFiles (files) -> write_files files ; exit 0
