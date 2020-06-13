903ea677b3a67b199212318ed78ceaafecbd62f8
Jesper Oskarsson <jesosk@gmail.com>
2020-06-13 12:15:59 +0200

    Moved more logic from Cli.ml

--- bootstrap/Cli.ml
+++ bootstrap/Cli.ml
@@ -10,10 +10,7 @@ let read_file m path =
     let file = really_input_string channel (in_channel_length channel) in
     close_in channel;
     CSourceFile (m, path, ml_string_to_indexed_iterator file);;
-let read_files m file_paths = list_45map (read_file m) file_paths;;
-let read_modules modules = let read_module m = (match m with CPair (module_name, path) -> read_file (CModulePath (module_name)) path)
-                            in list_45map read_module modules;;
-let with_preamble files = list_45cons (read_file CModuleSelf (string_45concat data_path (data_45preamble_45filename ()))) files;;
+let read_files files = list_45map (pair_45map read_file) files;;
 
 let write_file path content =
     let channel = open_out (reuse_string_to_ml path) in
@@ -28,5 +25,5 @@ while true do
           CCommandError (error) -> Printf.eprintf "%s\n" (reuse_string_to_ml error) ; exit 1
         | CCommandOutput (output) -> Printf.printf "%s" (reuse_string_to_ml output) ; exit 0
         | CCommandWriteFiles (files) -> write_files files ; exit 0
-        | CCommandReadFiles (file_paths, modules, state) -> current := CEventReadFiles (with_preamble (list_45concat (read_modules modules) (read_files CModuleSelf file_paths)), state)
+        | CCommandReadFiles (files, state) -> current := CEventReadFiles (read_files files, state)
 done
