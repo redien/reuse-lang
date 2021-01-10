1d224243e4ec566e47073c215df1a7c41212337b
Jesper Oskarsson <jesosk@gmail.com>
2021-01-10 18:44:39 +0100

    Equivalence test for dictionary

--- standard-library/specification/dictionary.hs
+++ standard-library/specification/dictionary.hs
@@ -1,27 +1,80 @@
+{-# LANGUAGE DeriveGeneric #-}
+import GHC.Generics
 import Test.QuickCheck
 import Test.QuickCheck.Modifiers (NonEmptyList (..))
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.Int
 import Data.Bifunctor
 import qualified ReuseStdlib as Reuse
 import Conversions
 
+data ReuseDictionary
+    = DEmpty
+    | DSet String Int32 ReuseDictionary
+    | DRemove String ReuseDictionary
+    deriving Generic
+
+instance Show ReuseDictionary where
+    show DEmpty = "(dictionary-empty)"
+    show (DSet k v xs) = "(dictionary-set " ++ (show k) ++ " " ++ (show v) ++ " " ++ (show xs) ++ ")"
+    show (DRemove k xs) = "(dictionary-remove " ++ (show k) ++ " " ++ (show xs) ++ ")"
+
+emptyGen :: Gen ReuseDictionary
+emptyGen = return DEmpty
+
+setGen :: Int -> Gen ReuseDictionary
+setGen n = do
+    k <- arbitrary
+    v <- arbitrary
+    xs <- arbitraryReuseDictionary (n - 1)
+    return (DSet k v xs)
+
+removeGen :: Int -> Gen ReuseDictionary
+removeGen n = do
+    k <- arbitrary
+    xs <- arbitraryReuseDictionary (n - 1)
+    return (DRemove k xs)
+
+arbitraryReuseDictionary :: Int -> Gen ReuseDictionary
+arbitraryReuseDictionary 0 = emptyGen
+arbitraryReuseDictionary n = frequency [(2, setGen n), (1, removeGen n)]
+
+instance Arbitrary ReuseDictionary where
+    arbitrary = do
+        x <- oneof [arbitraryReuseDictionary 1, arbitraryReuseDictionary 2, arbitraryReuseDictionary 3, sized arbitraryReuseDictionary]
+        return x
+    shrink x = shrinkToNil x ++ genericShrink x
+        where
+            shrinkToNil DEmpty = []
+            shrinkToNil _ = [DEmpty]
+
+evalReuse :: ReuseDictionary -> Reuse.Tdictionary Int32
+evalReuse DEmpty = Reuse.dictionary_45empty
+evalReuse (DSet k v xs) = Reuse.dictionary_45set (string_to_reuse k) v (evalReuse xs)
+evalReuse (DRemove k xs) = Reuse.dictionary_45remove (string_to_reuse k) (evalReuse xs)
+
+evalHs :: ReuseDictionary -> Map.Map String Int32
+evalHs DEmpty = Map.empty
+evalHs (DSet k v xs) = Map.insert k v (evalHs xs)
+evalHs (DRemove k xs) = Map.delete k (evalHs xs)
+
 prop_isomorphic :: Map.Map String Int32 -> Bool
 prop_isomorphic xs = dictionary_to_hs (dictionary_to_reuse xs) == xs
 
-prop_dictionary_set :: String -> Int32 -> Map.Map String Int32 -> Bool
-prop_dictionary_set s x xs = dictionary_to_hs (Reuse.dictionary_45set (string_to_reuse s) x (dictionary_to_reuse xs)) == Map.insert s x xs
+prop_equivalence :: ReuseDictionary -> Bool
+prop_equivalence xs = dictionary_to_hs (evalReuse xs) == evalHs xs
 
 prop_dictionary_get :: String -> Map.Map String Int32 -> Bool
 prop_dictionary_get k xs = maybe_to_hs (Reuse.dictionary_45get (string_to_reuse k) (dictionary_to_reuse xs)) == Map.lookup k xs
 
 prop_dictionary_entries :: Map.Map String Int32 -> Bool
-prop_dictionary_entries xs = map (bimap string_to_hs id) (list_pair_to_hs (Reuse.dictionary_45entries (dictionary_to_reuse xs))) == Map.toList xs
+prop_dictionary_entries xs = Set.fromList (map (bimap string_to_hs id) (list_pair_to_hs (Reuse.dictionary_45entries (dictionary_to_reuse xs)))) == Set.fromList (Map.toList xs)
 
 quickCheckN f = quickCheck (withMaxSuccess 100 f)
 
 main = do
     quickCheckN prop_isomorphic
-    quickCheckN prop_dictionary_set
+    quickCheck (withMaxSuccess 100000 prop_equivalence)
     quickCheckN prop_dictionary_get
     quickCheckN prop_dictionary_entries
