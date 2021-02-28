93c217fb5aaa399297c806d1d83330e460f0c6c9
Jesper Oskarsson <jesosk@gmail.com>
2021-02-28 19:26:07 +0100

    Embed module compiler into ocaml to make bootstrapping easier

--- parser/build.sh
+++ parser/build.sh
@@ -5,17 +5,17 @@ project_root=$(dirname "$0")/..
 
 build_dir=$($project_root/dev-env/builddir.sh parser)
 
-$project_root/reusec --language module\
-                     --output $build_dir/parser.reuse\
-                     $project_root/sexp-parser/sexp-parser.reuse\
-                     $project_root/string-gen/string-gen.reuse\
-                     $project_root/parser/ast.reuse\
-                     $project_root/parser/source-file.strings\
-                     $project_root/parser/source-file.reuse\
-                     $project_root/parser/scope.reuse\
-                     $project_root/parser/symbol-table.reuse\
-                     $project_root/parser/symbols.strings\
-                     $project_root/parser/symbols.reuse\
-                     $project_root/parser/parser-context.strings\
-                     $project_root/parser/parser-context.reuse\
-                     $project_root/parser/parser.reuse
+$project_root/bin/reuse-ocaml --language module\
+                              --output $build_dir/parser.reuse\
+                              $project_root/sexp-parser/sexp-parser.reuse\
+                              $project_root/string-gen/string-gen.reuse\
+                              $project_root/parser/ast.reuse\
+                              $project_root/parser/source-file.strings\
+                              $project_root/parser/source-file.reuse\
+                              $project_root/parser/scope.reuse\
+                              $project_root/parser/symbol-table.reuse\
+                              $project_root/parser/symbols.strings\
+                              $project_root/parser/symbols.reuse\
+                              $project_root/parser/parser-context.strings\
+                              $project_root/parser/parser-context.reuse\
+                              $project_root/parser/parser.reuse
--- standard-library/build.sh
+++ standard-library/build.sh
@@ -5,17 +5,18 @@ script_path=$(dirname "$0")
 project_root=$script_path/..
 build_dir=$($project_root/dev-env/builddir.sh standard-library)
 
-$project_root/bin/reuse-module --output $build_dir/standard-library.reuse\
-                               --stdlib false\
-                               $script_path/combinators.reuse\
-                               $script_path/boolean.reuse\
-                               $script_path/pair.reuse\
-                               $script_path/maybe.reuse\
-                               $script_path/iterable.reuse\
-                               $script_path/indexed-iterator.reuse\
-                               $script_path/list.reuse\
-                               $script_path/string.reuse\
-                               $script_path/result.reuse\
-                               $script_path/state.reuse\
-                               $script_path/array.reuse\
-                               $script_path/dictionary.reuse
+$project_root/bin/reuse-ocaml --language module\
+                              --output $build_dir/standard-library.reuse\
+                              --stdlib false\
+                              $script_path/combinators.reuse\
+                              $script_path/boolean.reuse\
+                              $script_path/pair.reuse\
+                              $script_path/maybe.reuse\
+                              $script_path/iterable.reuse\
+                              $script_path/indexed-iterator.reuse\
+                              $script_path/list.reuse\
+                              $script_path/string.reuse\
+                              $script_path/result.reuse\
+                              $script_path/state.reuse\
+                              $script_path/array.reuse\
+                              $script_path/dictionary.reuse
