66e7ecaa82f78e745ec1b9c16b060e20dc14ac2c
Jesper Oskarsson <jesosk@gmail.com>
2021-06-13 13:38:01 +0200

    Delete binaries first since copying over existing binaries sometimes lead to problems on macos m1

--- build.sh
+++ build.sh
@@ -18,16 +18,19 @@ ocamlopt -O3 unix.cmxa \
          -o "$project_root/bin/reuse-ocaml"
 
 $project_root/compiler-backend/module/build.sh
+[ -f "$project_root/bin/reuse-module" ] && rm "$project_root/bin/reuse-module"
 cp "$($project_root/dev-env/builddir.sh module-compiler)/compiler" "$project_root/bin/reuse-module"
 cp "$($project_root/dev-env/builddir.sh module-compiler)/data/pervasives.reuse" "$project_root/bin/data/pervasives.reuse"
 cp "$($project_root/dev-env/builddir.sh module-compiler)/data/preamble.reuse" "$project_root/bin/data/preamble.reuse"
 
 $project_root/compiler-backend/haskell/build.sh
+[ -f "$project_root/bin/reuse-haskell" ] && rm "$project_root/bin/reuse-haskell"
 cp "$($project_root/dev-env/builddir.sh haskell-compiler)/compiler" "$project_root/bin/reuse-haskell"
 cp "$($project_root/dev-env/builddir.sh haskell-compiler)/data/Pervasives.hs" "$project_root/bin/data/Pervasives.hs"
 cp "$($project_root/dev-env/builddir.sh haskell-compiler)/data/preamble.hs" "$project_root/bin/data/preamble.hs"
 
 $project_root/compiler-backend/javascript/build.sh
+[ -f "$project_root/bin/reuse-javascript" ] && rm "$project_root/bin/reuse-javascript"
 cp "$($project_root/dev-env/builddir.sh javascript-compiler)/compiler" "$project_root/bin/reuse-javascript"
 cp "$($project_root/dev-env/builddir.sh javascript-compiler)/data/pervasives.js" "$project_root/bin/data/pervasives.js"
 cp "$($project_root/dev-env/builddir.sh javascript-compiler)/data/preamble.js" "$project_root/bin/data/preamble.js"
