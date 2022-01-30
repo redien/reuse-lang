f7bf1f5fc7738a9d574daefecd54a55cbd9be667
Jesper Oskarsson <jesosk@gmail.com>
2022-01-30 09:59:44 +0100

    Remove newline character from javascript test script output

--- compiler-backend/javascript/compile-nostdlib-test.sh
+++ compiler-backend/javascript/compile-nostdlib-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-echo -e "#!/usr/bin/env node\n$(cat $1/$2)\nconsole.log(reuse_main ())\n" > "$1/$3"
+echo -e "#!/usr/bin/env node\n$(cat $1/$2)\nprocess.stdout.write((reuse_main ()).toString())\n" > "$1/$3"
 chmod +x "$1/$3"
--- compiler-backend/javascript/compile-stdin-test.sh
+++ compiler-backend/javascript/compile-stdin-test.sh
@@ -8,5 +8,5 @@ project_root=$script_path/../..
 printf "\
 #!/usr/bin/env node
 $(cat $1/$2)\n\
-console.log(reuse_string_to_js (reuse_main (new Int8Array(require('fs').readFileSync(0)))))\n" > "$1/$3"
+process.stdout.write(reuse_string_to_js (reuse_main (new Int8Array(require('fs').readFileSync(0)))))\n" > "$1/$3"
 chmod +x "$1/$3"
