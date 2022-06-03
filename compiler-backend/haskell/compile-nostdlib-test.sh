1b4ea7792d64d2f69efeed438320ad2967422f05
Jesper Oskarsson <jesosk@gmail.com>
2022-06-03 22:03:11 +0200

    Fix problem in haskell tests that added a newline

--- compiler-backend/haskell/compile-nostdlib-test.sh
+++ compiler-backend/haskell/compile-nostdlib-test.sh
@@ -3,5 +3,5 @@ set -e
 
 echo -e '#!/usr/bin/env runghc\n' > "$1/$3"
 cat "$1/$2" >> "$1/$3"
-echo -e '\nmain = Prelude.putStrLn (Prelude.show reuse_main)\n' >> "$1/$3"
+echo -e '\nmain = Prelude.putStr (Prelude.show reuse_main)\n' >> "$1/$3"
 chmod +x "$1/$3"
