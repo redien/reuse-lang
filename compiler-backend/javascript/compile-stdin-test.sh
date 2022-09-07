9917d949026b02299ba93beeec952356a43c7ddb
Jesper Oskarsson <jesosk@gmail.com>
2022-09-07 20:12:35 +0200

    env on linux does not accept arguments

--- compiler-backend/javascript/compile-stdin-test.sh
+++ compiler-backend/javascript/compile-stdin-test.sh
@@ -6,7 +6,7 @@ script_path=$(dirname "$0")
 project_root=$script_path/../..
 
 printf "\
-#!/usr/bin/env node --trace-uncaught\n" > "$1/$3"
+#!/usr/bin/env node\n" > "$1/$3"
 cat "$1/$2" >> "$1/$3"
 printf "\n\
 process.stdout.write(reuse_string_to_js (reuse_main (new Int8Array(require('fs').readFileSync(0)))))\n" >> "$1/$3"
